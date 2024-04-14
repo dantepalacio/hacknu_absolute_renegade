a75668d5828b565dc19e710c2ad05c73423e4342
Bismark_x <xxwoll4nxx@gmail.com>
2024-04-14 12:08:07 +0500

    all changes

--- beyimtech_hackhaton/beyimtech_hackhaton/settings.py
+++ beyimtech_hackhaton/beyimtech_hackhaton/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-
+from environ import Env
+env = Env()
+env.read_env()
+import dj_database_url
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,15 +86,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'beyimtech_hackhaton.wsgi.application'
 
-
+print(os.environ.get("DATABASE_URL"))
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': dj_database_url.config(default=env("DATABASE_URL"))}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
